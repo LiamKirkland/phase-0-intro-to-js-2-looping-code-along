a46ed13e14e69b8a75441117c43ab2d63fbee700
Liam Kirkland <liam.kirkland@liam.kirkland>
2026-02-18 18:24:09 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, giftType) {
+    let cards = []
+    for(let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+    }
+    return cards
+}
+
+function countDown(x) {
+    for(let i = x; i >= 0; i--) {
+        console.log(i)
+    }
+}
