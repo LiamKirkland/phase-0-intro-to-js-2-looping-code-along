06c3fc54499f8bdbcc7a33b1a7a144fd611dff71
LiamKirkland <liam.b.kirkland@gmail.com>
2026-05-05 15:42:53 -0400

    Practice Loops

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(start) {
+    while(start >= 0) {
+        console.log(start);
+        start--;
+    }
+}
+
+function writeCards(arr, giftType) {
+    const cards = [];
+    for(let card of arr) {
+        console.log(`Thank you, ${card}, for the wonderful ${giftType} gift!`);
+        cards.push(`Thank you, ${card}, for the wonderful ${giftType} gift!`);
+    }
+
+    return cards;
+}
